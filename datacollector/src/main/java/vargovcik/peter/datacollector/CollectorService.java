275902f010f11efd3d3416777c8dfe0e71413852
Peter Vargovcik <peter.vargovcik@ericsson.com>
2019-12-13 16:08:07 +0000

    test

--- datacollector/src/main/java/vargovcik/peter/datacollector/CollectorService.java
+++ datacollector/src/main/java/vargovcik/peter/datacollector/CollectorService.java
@@ -4,14 +4,13 @@ package vargovcik.peter.datacollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 public class CollectorService extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
