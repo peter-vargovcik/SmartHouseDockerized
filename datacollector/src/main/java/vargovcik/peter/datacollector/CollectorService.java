a04536f312546035010000b88cb616d5b82d44a6
Peter Vargovcik <peter.vargovcik@ericsson.com>
2019-12-13 14:57:26 +0000

    added autoconfing

--- datacollector/src/main/java/vargovcik/peter/datacollector/CollectorService.java
+++ datacollector/src/main/java/vargovcik/peter/datacollector/CollectorService.java
@@ -2,16 +2,25 @@ package vargovcik.peter.datacollector;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class CollectorService {
+@EnableAutoConfiguration
+public class CollectorService extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CollectorService.class);
 
         System.out.println("Starting CollectorService !!");
+    }
+
+    @Override
+    protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(CollectorService.class);
     }
 }
